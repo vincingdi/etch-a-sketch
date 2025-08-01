7ae04b26b61489fcf54df233e964535e1e2b3adb
Vin <110591050+vincingdi@users.noreply.github.com>
2025-08-01 09:00:56 +0300

    Implement grid creation and random color functionality; add CSS styling

--- script.js
+++ script.js
@@ -1,20 +1,69 @@
-// variable put html div container in variable using his div
-const container = document.querySelector("#container");
-
-// container.style.display = "flex";
-// 16 X 16 grid
+function createGrid(squaresPerSide = 16) {
 
 
-// How to make a every 16th div on separate line.
+    // variable put html div container in variable using his div
+    // const container = document.querySelector("#container");
+    var container = document.getElementById("container");
+    container.innerHTML = '';
+    container.style.display = "flex";
+    container.style.flexWrap = "wrap";
+    container.style.width = "640px";
+    container.style.height = "640px";
+    // 16 X 16 grid
 
-// append too container 16 div but make them as a grid with css by making container display:flex 
-
-const div = document.createElement("div");
-div.style.backgroundColor = "grey";
 
 
-for (let i=0; i<16; i++) {
-   container.appendChild(div);
+    // append too container 16 div but make them as a grid with css by making container display:flex 
+
+    for (let i=0; i<squaresPerSide*squaresPerSide; i++) {
+
+    var div = document.createElement("div");
+    div.style.width = 640 / squaresPerSide + "px";
+    // console.log(div.style.width);
+    div.style.height = 640 / squaresPerSide + "px";
+    console.log(div.style.height);
+    div.style.backgroundColor  = "gray";
+    div.classList.add("grid-block");
+    container.appendChild(div);
+
+    }
+
+    gridBlocks = document.querySelectorAll(".grid-block");
+
+    gridBlocks.forEach(block => {
+        block.addEventListener("mouseover", () =>{
+            block.style.backgroundColor = randomizeColor();
+        });
+
+    });
 }
 
 
+function randomizeColor() {
+    // randomize r g b values and but them in variables 
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    // return rgb color value 
+    return color;
+
+}
+
+
+function mainLoop() {
+
+    createGrid();
+    let squaresPerSide;
+    document.getElementById("size-button").onclick = function() {
+        while (!(Number(squaresPerSide) > 0 && Number(Number(squaresPerSide) <= 100))) {
+            squaresPerSide = prompt("How many squares per side?");
+        } 
+        createGrid(squaresPerSide);
+
+    }
+    
+    
+}
+
+mainLoop(); 
